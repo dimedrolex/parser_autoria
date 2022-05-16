53b93dba2e5eab18051964375b8f45e2733c4b64
dimedrolex <dimaxfronin@gmail.com>
2022-05-17 00:56:08 +0300

    update parser

--- auto_parse.py
+++ auto_parse.py
@@ -18,8 +18,20 @@ def get_html(url, params=None):
 def get_content(html):
     soup = BeautifulSoup(html, 'html.parser')
     items = soup.find_all('section', class_='ticket-item')
-    print(items)
-    
+    # print(items)
+    cars = []
+    for item in items[:3]:
+        cars.append({
+            'title': item.find('a', class_='address').get_text(strip=True),
+            'link': HOST + item.find('a', class_='address').get('href'),
+            'usd_price': item.find('span', class_='bold green size22').get_text(strip=True) + '$',
+            'uah_price': item.find('span', class_='i-block').get_text(strip=True),
+            'picture': item.find('a', class_='photo-185x120').get('href'),
+            'city': item.find('li', class_ = 'item-char view-location js-location').get_text(strip=True),
+        })
+    for present in cars:
+        print(f'{present["title"]} -> Link: {present["link"]} \nPicture: {present["picture"]}\nLocation: {present["city"]}\nPrice_USD: {present["usd_price"]} == Price_UAH: {present["uah_price"]}\n')
+
 
 # Основная функция парсинга страницы
 def parse():
